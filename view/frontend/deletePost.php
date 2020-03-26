963660c2fca8f5fee59aab6d4928b6ff438c44d5
jonathan massoué <j.massoue@hotmail.fr>
2020-03-26 11:51:44 +0100

    deletepostetcomment

---
+++ view/frontend/deletePost.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> <!-- view -->
+<html lang="fr">
+
+	<head>
+		<?php include("view/include/head.php"); ?>
+	</head>
+
+	<body>
+		
+		<header>
+			 <?php include("view/include/menu.php"); ?>
+		</header>
+
+		<p><a href ="index.php">Retour à la liste des chapitres</a></p>
+		<p> L'article a bien été supprimer </p>
+
+		<footer>
+			<?php include("view/include/footer.php"); ?>
+		</footer>
+    </body>
+</html>
